01c6d220aa905fa8aa88b3aed99d9607c0a54caa
Wycctrujjla109121 <56939296+Wycctrujjla109121@users.noreply.github.com>
2024-02-13 20:49:22 +0300

    fix: исправил classNames на class

--- src/modules/Footer/Footer.jsx
+++ src/modules/Footer/Footer.jsx
@@ -5,63 +5,63 @@ import { Privacy } from '../../components';
 
 export function Footer() {
     return (
-        <div className='wrapper'>
-            <div className='footer'>
-                <div className='footer__info'>
-                    <div className='links'>
-                        <div className='links__item'>
-                            <h2 className='links__title'>Каталог</h2>
-                            <div className='links__list'>
-                                <a className='links__link' href="#">Магазин</a>
-                                <a className='links__link' href="#">Линейки</a>
-                                <a className='links__link' href="#">Коллекции</a>
-                                <a className='links__link' href="#">Lookbook</a>
+        <div class='wrapper'>
+            <div class='footer'>
+                <div class='footer__info'>
+                    <div class='links'>
+                        <div class='links__item'>
+                            <h2 class='links__title'>Каталог</h2>
+                            <div class='links__list'>
+                                <a class='links__link' href="#">Магазин</a>
+                                <a class='links__link' href="#">Линейки</a>
+                                <a class='links__link' href="#">Коллекции</a>
+                                <a class='links__link' href="#">Lookbook</a>
                             </div>
                         </div>
-                        <div className='links__item'>
-                            <h2 className='links__title'>инфо</h2>
-                            <div className='links__list'>
-                                <a className='links__link' href="#">О бренде</a>
-                                <a className='links__link' href="#">Контакты</a>
-                                <a className='links__link' href="#">Доставка и оплата</a>
-                                <a className='links__link' href="#">обмен и возврат</a>
-                                <a className='links__link links__link_uper' href="#">FAQ</a>
+                        <div class='links__item'>
+                            <h2 class='links__title'>инфо</h2>
+                            <div class='links__list'>
+                                <a class='links__link' href="#">О бренде</a>
+                                <a class='links__link' href="#">Контакты</a>
+                                <a class='links__link' href="#">Доставка и оплата</a>
+                                <a class='links__link' href="#">обмен и возврат</a>
+                                <a class='links__link links__link_uper' href="#">FAQ</a>
                             </div>
                         </div>
                     </div>
 
-                    <div className='contacts'>
-                        <div className='contacts__contact'>
-                            <a href="tel:+7 919 96-99-95" className='contacts__link'>+7 919 96-99-95</a>
-                            <p className='contacts__text'>Звоните</p>
+                    <div class='contacts'>
+                        <div class='contacts__contact'>
+                            <a href="tel:+7 919 96-99-95" class='contacts__link'>+7 919 96-99-95</a>
+                            <p class='contacts__text'>Звоните</p>
                         </div>
 
-                        <div className='contacts__contact'>
-                            <a href="mailto:kukoyaka.store@yandex.ru" className='contacts__link'>kukoyaka.store@yandex.ru</a>
-                            <p className='contacts__text'>Пишите</p>
+                        <div class='contacts__contact'>
+                            <a href="mailto:kukoyaka.store@yandex.ru" class='contacts__link'>kukoyaka.store@yandex.ru</a>
+                            <p class='contacts__text'>Пишите</p>
                         </div>
 
-                        <div className='contacts__contact'>
-                            <a href="@vasilinciagashop" className='contacts__link'>@vasilinciagashop</a>
-                            <p className='contacts__text'>Следите в телеграм</p>
+                        <div class='contacts__contact'>
+                            <a href="@vasilinciagashop" class='contacts__link'>@vasilinciagashop</a>
+                            <p class='contacts__text'>Следите в телеграм</p>
                         </div>
                     </div>
 
-                    <div className='footer__privacy'>
-                        <p className='footer__privacy_text'>Подписывайтесь, чтобы узнавать о новых коллекциях, поступлениях и всем-всем-всем раньше всех-всех-всех!</p>
+                    <div class='footer__privacy'>
+                        <p class='footer__privacy_text'>Подписывайтесь, чтобы узнавать о новых коллекциях, поступлениях и всем-всем-всем раньше всех-всех-всех!</p>
                         <Privacy />
                     </div>
                 </div>
-                <div className='shop'>
-                    <div className='shop__info'>
-                        <p className='shop__text'>ИП Кондратьева Людмила Юрьевна</p>
-                        <p className='shop__text'>ИНН 183403484917 </p>
+                <div class='shop'>
+                    <div class='shop__info'>
+                        <p class='shop__text'>ИП Кондратьева Людмила Юрьевна</p>
+                        <p class='shop__text'>ИНН 183403484917 </p>
                     </div>
-                    <div className='shop__links'>
-                        <a href="#" className='shop__link'>Не является публичной офертой</a>
-                        <a href="#" className='shop__link'>Политика конфиденциальности</a>
-                        <a href="#" className='shop__link'>Сертификат соответствия</a>
-                        <a href="#" className='shop__link'>Использование cookie</a>
+                    <div class='shop__links'>
+                        <a href="#" class='shop__link'>Не является публичной офертой</a>
+                        <a href="#" class='shop__link'>Политика конфиденциальности</a>
+                        <a href="#" class='shop__link'>Сертификат соответствия</a>
+                        <a href="#" class='shop__link'>Использование cookie</a>
                     </div>
                 </div>
             </div>
--- src/modules/Product/Product.jsx
+++ src/modules/Product/Product.jsx
@@ -10,17 +10,17 @@ export function Product() {
             <img class='product__img' src={img} alt='Лонгслив Moto' />
 
             <div class='info'>
-                <div className='info__list'>
+                <div class='info__list'>
                     <h2 class='info__title'>Лонгслив Moto</h2>
                     <div class='info__prices'>
                         <p class='info__price'>5 700 руб.</p>
                         <p class='info__price info__price_new'>4 200 руб.</p>
                     </div>
                 </div>
-                <div className='info__sizes'>
-                    <p className='info__size'>XS</p>
-                    <p className='info__size'>S–M</p>
-                    <p className='info__size info__size_disable'>L–XL</p>
+                <div class='info__sizes'>
+                    <p class='info__size'>XS</p>
+                    <p class='info__size'>S–M</p>
+                    <p class='info__size info__size_disable'>L–XL</p>
                 </div>
             </div>
         </div>
