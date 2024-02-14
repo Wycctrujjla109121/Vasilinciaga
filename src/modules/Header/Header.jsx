145d6e101dee60e7fc30db1b0c4e1ac5307e74dc
Wycctrujjla109121 <56939296+Wycctrujjla109121@users.noreply.github.com>
2024-02-14 18:33:03 +0300

    feat: добавил в header текст

--- src/modules/Header/Header.jsx
+++ src/modules/Header/Header.jsx
@@ -5,7 +5,10 @@ import './Header.scss'
 export function Header() {
     return (
         <header class='header'>
-            <a href="/" class='header__link'>KUKOYAKA</a>
+            <div class='header__info'>
+                <a href="/" class='header__link'>KUKOYAKA</a>
+                <p class='header__text'>Магазин</p>
+            </div>
             <div class='navigation'>
                 <div class='navigation__list'>
                     <a href="#" class='navigation__link navigation__link_uper'>NEW DROP</a>
